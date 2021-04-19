bcd58eae7d79db9785445732e2ca367ff00da4cc
Rafi-Maul <72507875+Rafi-Maul@users.noreply.github.com>
2021-04-19 16:59:41 +0700

    uts

---
+++ header.php
@@ -0,0 +1,49 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="carousel slide" id="carousel-353789">
+			<ol class="carousel-indicators">
+				<li data-slide-to="0" data-target="#carousel-353789" class="active">
+				</li>
+				<li data-slide-to="1" data-target="#carousel-353789">
+				</li>
+				<li data-slide-to="2" data-target="#carousel-353789">
+				</li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" alt="Carousel Bootstrap First" src="img/nfl.jpg">
+					<div class="carousel-caption">
+						<h4>
+							First Thumbnail label
+						</h4>
+						<p>
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" alt="Carousel Bootstrap Second" src="img/nba.jpg">
+					<div class="carousel-caption">
+						<h4>
+							Second Thumbnail label
+						</h4>
+						<p>
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" alt="Carousel Bootstrap Third" src="img/mdr2.jpg">
+					<div class="carousel-caption">
+						<h4>
+							Third Thumbnail label
+						</h4>
+						<p>
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+					</div>
+				</div>
+			</div> <a class="carousel-control-prev" href="#carousel-353789" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-353789" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+		</div>
+	</div>
+</div>
